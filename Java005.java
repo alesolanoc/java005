57eec34336855c3cc83990ebb4fd9936f704c168
alejandro <alejandro.solano@jalasoft.com>
2023-06-11 18:28:08 -0400

    java005 added

---
+++ Java005.java
@@ -0,0 +1,14 @@
+class Java005{
+	public static void main(String args[]){
+		int value1 = 10;
+		boolean value2 = true;
+		float value4 = 2.3f;
+		char value5 = 'a';
+		String value6 = "asdfasdfasdf";
+		System.out.println(value1);
+		System.out.println(value2);
+		System.out.println(value6);
+		System.out.println(value4);
+		System.out.println(value5);
+	}
+}
